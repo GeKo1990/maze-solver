c18fd29cae6f9b4d65f551d3262318b2c89c64c6
GeKo1990 <geko1990@gmx.at>
2024-07-08 17:42:07 +0200

    Added Point and Line classes; Enhanced Window class to draw lines
    - Implemented `Point` class to represent coordinates with `x` and `y` attributes.
    - Implemented `Line` class with `start` and `end` attributes, ensuring they are instances of `Point`.
    - Enhanced `Window` class to include a `draw_line` function that draws lines on the canvas.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 from window import Window
+from line import Line
+from point import Point
 
 if __name__ == "__main__":
     window = Window(800, 600)
+
+    # testing draw line
+    window.draw_line(Line(Point(0,0), Point(15,20)), "black")
+    window.draw_line(Line(Point(30,30), Point(15,20)), "red")
+
     window.wait_for_close()
--- src/window.py
+++ src/window.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+from line import Line
 
 class Window:
     def __init__(self, width, height):
@@ -12,7 +13,6 @@ class Window:
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
@@ -24,3 +24,6 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.__canvas, fill_color)
